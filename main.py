b6fab238275c955442aa0c9489dfa848e0d839a6
Aleksandr Tolkanev <allcars61@gmail.com>
2023-04-27 11:05:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 
 
 def get_photos_info(vk_user_id):
-    vk_session = vk_api.VkApi(token='YOUR_TOKEN')  # 'YOUR_TOKEN' заменить на токен ВК
+    vk_session = vk_api.VkApi(token="YOUR_TOKEN")  # 'YOUR_TOKEN' заменить на токен ВК
     vk = vk_session.get_api()
 
     try:
@@ -15,37 +15,43 @@ def get_photos_info(vk_user_id):
         print("Ошибка! Пользователь не найден.")
         sys.exit()
 
-    user_name = user_info['first_name'] + '_' + user_info['last_name']
-    photos = vk.photos.get(owner_id=vk_user_id, album_id='profile', photo_sizes=1, extended=1)['items']
+    user_name = user_info["first_name"] + "_" + user_info["last_name"]
+    photos = vk.photos.get(
+        owner_id=vk_user_id, album_id="profile", photo_sizes=1, extended=1
+    )["items"]
     photos_info = {}
 
     for i in range(len(photos)):
-        photo = sorted(photos[i]['sizes'], key=lambda x: (x['width'], x['height']), reverse=True)[0]
-        url = photo['url']
-        likes = str(photos[i]['likes']['count'])
-        date = datetime.utcfromtimestamp(photos[i]['date']).strftime('%Y-%m-%d_%H-%M-%S')
-        file_name = likes + '_' + date + '.jpg'
-        size = photo['type']
+        photo = sorted(
+            photos[i]["sizes"], key=lambda x: (x["width"], x["height"]), reverse=True
+        )[0]
+        url = photo["url"]
+        likes = str(photos[i]["likes"]["count"])
+        date = datetime.utcfromtimestamp(photos[i]["date"]).strftime(
+            "%Y-%m-%d_%H-%M-%S"
+        )
+        file_name = likes + "_" + date + ".jpg"
+        size = photo["type"]
 
         if likes in photos_info:
-            likes += '_' + date
+            likes += "_" + date
 
-        photos_info[likes] = {'url': url, 'size': size}
+        photos_info[likes] = {"url": url, "size": size}
 
     return photos_info, user_name
 
 
 def save_photos_to_disk(photos_info, user_name, yandex_disk_token):
-    headers = {'Authorization': 'OAuth ' + yandex_disk_token}
-    folder_name = user_name + '_photos_from_vk'
-    upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+    headers = {"Authorization": "OAuth " + yandex_disk_token}
+    folder_name = user_name + "_photos_from_vk"
+    upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
 
     try:
         requests.put(
-            'https://cloud-api.yandex.net/v1/disk/resources',
+            "https://cloud-api.yandex.net/v1/disk/resources",
             headers=headers,
-            params={'path': folder_name},
-            timeout=10
+            params={"path": folder_name},
+            timeout=10,
         )
     except:
         print("Ошибка при создании папки на Яндекс.Диске")
@@ -54,26 +60,26 @@ def save_photos_to_disk(photos_info, user_name, yandex_disk_token):
     photos_data = []
 
     for likes in photos_info:
-        url = photos_info[likes]['url']
-        file_name = likes + '.jpg'
+        url = photos_info[likes]["url"]
+        file_name = likes + ".jpg"
 
         response = requests.get(url)
 
-        files = {'file': (file_name, response.content)}
+        files = {"file": (file_name, response.content)}
         params = {
-            'url': upload_url,
-            'method': 'PUT',
-            'path': folder_name + '/' + file_name
+            "url": upload_url,
+            "method": "PUT",
+            "path": folder_name + "/" + file_name,
         }
 
         response = requests.get(upload_url, headers=headers, params=params)
 
-        photos_data.append({'file_name': file_name, 'likes': likes})
+        photos_data.append({"file_name": file_name, "likes": likes})
 
-    with open('photos_info.json', 'w', encoding='utf-8') as file:
+    with open("photos_info.json", "w", encoding="utf-8") as file:
         json.dump(photos_data, file, ensure_ascii=False, indent=4)
 
-    print('Готово!')
+    print("Готово!")
 
 
 def main():
@@ -84,5 +90,5 @@ def main():
     save_photos_to_disk(photos_info, user_name, yandex_disk_token)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
